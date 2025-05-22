6a1708bcb4a04ecfc1e68b2062da463de490eb87
Ranjeeta R Golabhavi <ranjeetargolabhavi@gmail.com>
2025-05-22 14:48:43 +0530

    day 2

---
+++ 6.cpp
@@ -0,0 +1,28 @@
+Class: Bus
+Real-life Scenario: Each bus has details like number, route, and capacity.
+
+#include<iostream>
+using namespace std;
+
+class Bus {
+public:
+    string busNumber;
+    string route;
+    int capacity;
+
+    void display() {
+        cout << "Bus Number: " << busNumber << endl;
+        cout << "Route: " << route << endl;
+        cout << "Capacity: " << capacity << " passengers" << endl;
+    }
+};
+
+int main() {
+    Bus b1;
+    b1.busNumber = "KA05 1234";
+    b1.route = "City Center to Airport";
+    b1.capacity = 40;
+
+    b1.display();
+    return 0;
+}
